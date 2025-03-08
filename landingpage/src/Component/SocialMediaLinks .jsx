a256c21b7a13b6036b97489d9d00dcbd673b7d3f
vansh432 <vanshdeep703@gmail.com>
2025-03-08 19:20:39 +0530

    chnage done

--- landingpage/src/Component/SocialMediaLinks .jsx
+++ landingpage/src/Component/SocialMediaLinks .jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { FaFacebook, FaLinkedin, FaYoutube } from "react-icons/fa";
 import { SiWhatsapp } from "react-icons/si";
 import { IoCall } from "react-icons/io5";
@@ -7,7 +7,7 @@ import { NavLink } from "react-router-dom";
 import axios from 'axios'
 const SocialMediaLinks = () => {
   const BASE_URL=import.meta.env.VITE_APP_API_BASE_URL
-  const apps = [
+  const [formData,setFormData]=useState([
     {
       name: "Facebook",
       icon: <FaFacebook className="text-blue-500" />,
@@ -44,20 +44,64 @@ const SocialMediaLinks = () => {
       borderColor: "#34D399",
       link: "",
     },
-  ];
+  ])
+
 
    useEffect(()=>{
-    
+    console.log(BASE_URL);
     async function getFetchData(){
+    try{
     const response=await axios.get(`${BASE_URL}/get-SocialMediaUrl/67cc3984626d90e6a28e6119`);
     console.log(response)
+    const socialData=response.data;
+    setFormData([
+      {
+        name: "Facebook",
+        icon: <FaFacebook className="text-blue-500" />,
+        borderColor: "#1877F2",
+        link:socialData?.socialMediaUrl?.facebook_Url,
+      },
+      {
+        name: "Instagram",
+        icon: <img src={instagram} alt="" className="h-6 w-6" />,
+        borderColor: "#E1306C",
+        link: socialData?.socialMediaUrl?.instagram_Url,
+      },
+      {
+        name: "WhatsApp",
+        icon: <SiWhatsapp className="text-green-400" />,
+        borderColor: "#25D366",
+        link: socialData?.socialMediaUrl?.whatsApp_Url,
+      },
+      {
+        name: "LinkedIn",
+        icon: <FaLinkedin className="text-blue-400" />,
+        borderColor: "#0077B5",
+        link:socialData?.socialMediaUrl?.linkedln_Url,
+      },
+      {
+        name: "YouTube",
+        icon: <FaYoutube className="text-red-500" />,
+        borderColor: "#FF0000",
+        link: socialData?.socialMediaUrl?.youtube_Url,
+      },
+      {
+        name: "Call",
+        icon: <IoCall className="text-green-400" />,
+        borderColor: "#34D399",
+        link: socialData?.socialMediaUrl?.facebook_Url,
+      },
+    ])
+    }catch(error){
+      console.log(error)
+    }
     }
   getFetchData();
    },[])
 
   return (
     <div id="social" className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-4 mt-10 p-5">
-      {apps.map((app, index) => (
+      {formData.map((app, index) => (
         <NavLink
           to={app?.link}
           key={index}
